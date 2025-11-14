b46cc1458816a48bc3b9ae25ba0bf825c3c29dd4
Aleksander Belov <bsaska86@gmail.com>
2025-11-14 23:33:26 +0300

    add and edit some blocks, add slider(without mobile version)

---
+++ scripts/slaider.js
@@ -0,0 +1,16 @@
+let swiper = new Swiper('.swiper-container', {
+  loop: true,  // Зацикливаем слайды
+  // navigation: {
+  //   nextEl: '.swiper-button-next',  // Кнопка "следующий"
+  //   prevEl: '.swiper-button-prev',  // Кнопка "предыдущий"
+  // },
+
+  autoplay: {
+    delay: 3000,  // Автоматический переход через 2.5 сек
+    disableOnInteraction: false,  // Автоплей не остановится при взаимодействии
+  },
+  speed: 750,  // Скорость анимации
+  slidesPerView: 1,  // Показать только один слайд
+  spaceBetween: 10,  // Нет промежутков между слайдами
+  effect: 'slide',  // Эффект перехода между слайдами
+});
